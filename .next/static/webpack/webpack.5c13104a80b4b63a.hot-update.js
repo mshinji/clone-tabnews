a52e6e1bc534af07d3e8f7a4aa7335d7995fdde3
mShinji <176821538+mshinji@users.noreply.github.com>
2024-12-10 10:17:47 -0300

    test

--- .next/static/webpack/webpack.5c13104a80b4b63a.hot-update.js
+++ .next/static/webpack/webpack.5c13104a80b4b63a.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a75d87f7d4ccc9a1"; }
+/******/ 	__webpack_require__.h = function() { return "4d87fd56da4df1d7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
